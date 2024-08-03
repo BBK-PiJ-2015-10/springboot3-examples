7157418ac12c783a97bfaadebed1485d4ab43282
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2024-08-03 11:39:26 -0400

    adding depn

---
+++ examples/src/test/java/com/learing/springboot3/examples/ExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learing.springboot3.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
