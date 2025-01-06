43a2d659253b3629fe23be4aa210446888b79a80
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-01-06 12:18:41 -0500

    adding new project

---
+++ spring-cloud-stream-aca-processor/src/main/java/tr/springcloudstreamacaprocessor/SpringCloudStreamAcaProcessorApplication.java
@@ -0,0 +1,13 @@
+package tr.springcloudstreamacaprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamAcaProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStreamAcaProcessorApplication.class, args);
+	}
+
+}
