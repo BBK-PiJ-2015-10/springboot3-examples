3f91f91eb26667102b8ad6cdeabc9989c398693d
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-01-12 12:29:26 -0500

    adding properties

--- spring-rabbit-csv/src/main/java/tr/springcloudrabbitcsv/SpringCloudRabbitCsvApplication.java
+++ spring-rabbit-csv/src/main/java/tr/springcloudrabbitcsv/SpringCloudRabbitCsvApplication.java
@@ -3,6 +3,12 @@ package tr.springcloudrabbitcsv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// TODO:
+// 1 - Change msg to json
+// https://thepracticaldeveloper.com/produce-and-consume-json-messages-with-spring-boot-amqp/
+// 2 - Read msg from queue and write to file
+
 @SpringBootApplication
 public class SpringCloudRabbitCsvApplication {
 
