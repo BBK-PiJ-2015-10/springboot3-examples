8c1b5e2354dfd64e617c6a108f50a8b324e23725
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-01-13 08:14:22 -0500

    consuming one message

--- spring-rabbit-csv/src/main/java/tr/springcloudrabbitcsv/controller/MessageBrokerController.java
+++ spring-rabbit-csv/src/main/java/tr/springcloudrabbitcsv/controller/MessageBrokerController.java
@@ -2,6 +2,7 @@ package tr.springcloudrabbitcsv.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,12 +23,19 @@ public class MessageBrokerController {
         this.csvFileReader = csvFileReader;
     }
 
-    @PostMapping(path = "/publish/queue/{queueName}")
+    @PostMapping(path = "/queue/{queueName}")
     public void publishMessage(@PathVariable("queueName") String queueName) {
         logger.info("Controller received eligibility request to published messages to queue: {}", queueName);
         var requests = csvFileReader.readCsvFile();
         requests.forEach(er -> queuePublisher.sendMessage(queueName, er));
         logger.info("Controller published eligibility request to queue");
+    }
+
+    @GetMapping(path = "/queue/{queueName}")
+    public void fetchMessages(@PathVariable("queueName") String queueName) {
+        logger.info("Controller received request to consume messages to queue: {}", queueName);
+        queuePublisher.fetchMessages(queueName);
+        logger.info("Controller processed eligibility request to consume from queue {}", queueName);
     }
 
 }
--- spring-rabbit-csv/src/main/java/tr/springcloudrabbitcsv/publisher/QueuePublisher.java
+++ spring-rabbit-csv/src/main/java/tr/springcloudrabbitcsv/publisher/QueuePublisher.java
@@ -3,8 +3,11 @@ package tr.springcloudrabbitcsv.publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import tr.springcloudrabbitcsv.entity.EligibilityRequest;
+
+import java.util.List;
 
 import static tr.springcloudrabbitcsv.config.RabbitMQConfig.EXCHANGE_NAME;
 import static tr.springcloudrabbitcsv.config.RabbitMQConfig.QUEUE_NAME;
@@ -29,6 +32,19 @@ public class QueuePublisher {
         }
         rabbitTemplate.convertAndSend(EXCHANGE_NAME, QUEUE_NAME, eligibilityRequest);
         logger.info("Message {} sent to queue: {}", eligibilityRequest, queueName);
+    }
+
+    public void fetchMessages(String queueName) {
+        var validQueue = queueName.equals(QUEUE_NAME);
+        if (!validQueue) {
+            logger.error("Invalid queue name: {}", queueName);
+            return;
+        }
+        var response = rabbitTemplate.receiveAndConvert(queueName);
+        logger.info("WOOF {}",response.toString());
+        //List<EligibilityRequest> response = rabbitTemplate.receiveAndConvert(queueName, new ParameterizedTypeReference<List<EligibilityRequest>>() {
+        //});
+        logger.info("Consumed from queue {} messages : {}", queueName, response);
     }
 
 }
